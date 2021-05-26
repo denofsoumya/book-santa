0db6c6e6eb9ca2e00854e609485b29a2c4b0db4a
Soumya Xavier <denofsoumya@gmail.com>
2021-05-26 19:54:49 +0530

    first commit

---
+++ components/AppDrawerNavigator.js
@@ -0,0 +1,28 @@
+import {createDrawerNavigator} from 'react-navigation-drawer';
+import {AppTabNavigator} from './AppTabNavigator';
+import CustomSideBarMenu from './CustomSideBarMenu';
+import SettingScreen from '../screens/SettingScreen';
+import MyDonationScreen from '../screens/MyDonationScreen';
+import NotificationScreen from '../screens/NotificationScreen';
+
+export const AppDrawerNavigator =createDrawerNavigator({
+    Home: {
+        screen: AppTabNavigator
+    },
+    Settings: {
+        screen: SettingScreen
+    },
+    MyDonations: {
+        screen: MyDonationScreen
+    },
+    Notifications: {
+        screen: NotificationScreen
+    },
+},
+{
+    contentComponent: CustomSideBarMenu
+},
+{
+    initialRouteName: 'Home'
+}
+)
